4f31e1b99e14aca011164a5dd91e773037a2975e
csslick <digitone@naver.com>
2021-04-03 15:09:28 +0900

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Table from './components/Table';
 
 function App() {
+  async function fetch_data() {
+    const url = 'https://api.bithumb.com/public/ticker/ALL';
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      // console.log(data);
+      const coin_data = [];
+      for(let key in data.data) {
+        coin_data.push({'name': key, ...data.data[key]});
+      }
+      console.log(coin_data)
+    }catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetch_data();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Header />
+      {/* <Table /> */}
+    </main>
   );
 }
 
